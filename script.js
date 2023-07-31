ca70edba673d24d0200e03059912e0e2b36fbd50
Nihan Sahin <nihansahin@Hulusis-MacBook-Pro.local>
2023-07-31 17:58:35 +0300

    added changes

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-"use strict";
+const allLists = document.querySelector('.all-lists')
+const favorites = document.querySelector('.favorite-lists')
+const lastAdded = document.querySelector('.last-added-items')
 
 const budget = document.querySelector(".budget").textContent;
 
 document.querySelector(".check").addEventListener("click", function () {
   const budget = Number(document.querySelector(".budget").value);
   console.log(budget, typeof budget);
-});
+})
+
+const elements = document.querySelector(".elements").classList.add('hidden')
 
