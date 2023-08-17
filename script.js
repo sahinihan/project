3ec0b3a952bc11909c5929a7de976710c1d8b135
Nihan Sahin <nihansahin@Hulusis-MBP.home>
2023-08-17 17:24:21 +0300

    function for adding prices and alerting when over budget

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const removeHidden = function (arr) {
 //hidden content when restarted
 hide([".known-budget", ".change-known-budget", ".changing-the-budget"]);
 hide([".p-list-name", ".list-name", ".list-name-check", ".List-Name"]);
+hide([".Food", ".Health", ".Clothing", ".Cosmetics", ".Other"]);
 hide([".list-item", ".last-added-items"]);
 hide([".save-list"]);
 
@@ -19,12 +20,15 @@ let budget = document.querySelector(".budget-number");
 let budgetInfo = document.querySelector(".known-budget").textContent;
 let changeBudget = document.querySelector(".changing-the-budget");
 let btnChange = document.querySelector(".change-known-budget");
+let budgetAmount = 0;
+let itemPrices = {};
 
 // recieving initial budget and portraying it
 const assignBudget = document
   .querySelector(".budget-check")
   .addEventListener("click", function () {
     let budget = Number(document.querySelector(".budget-number").value);
+    budgetAmount = Number(document.querySelector(".budget-number").value);
     document.querySelector(".known-budget").classList.remove("hidden");
     document.querySelector(
       ".known-budget"
@@ -43,7 +47,6 @@ const change = (budget = document
       .querySelector(".changed-budget-check")
       .addEventListener("click", function () {
         let budget = Number(document.querySelector(".changed-budget").value);
-        console.log(budget);
         document.querySelector(
           ".known-budget"
         ).textContent = `${budgetInfo} ${budget} Turkish liras `;
@@ -61,7 +64,7 @@ const assignName = document
   .querySelector(".btn-create-list")
   .addEventListener("click", function () {
     removeHidden([".p-list-name", ".list-name", ".list-name-check"]);
-    hide([".btn-create-list"]);
+    hide([".btn-create-list", ".create-list-title"]);
   });
 
 const ShowListName = document
@@ -76,144 +79,156 @@ const ShowListName = document
 
 // adding categories
 const categoryList = document.querySelector(".categories-list");
+function createCategoriesF() {
+  let food = document.createElement("option");
+  food.textContent = "Food";
+  categoryList.add(food);
 
-let food = document.createElement("option");
-food.textContent = "Food";
-categoryList.add(food);
+  let health = document.createElement("option");
+  health.textContent = "Health";
+  categoryList.add(health);
 
-let health = document.createElement("option");
-health.textContent = "Health";
-categoryList.add(health);
+  let cosmetics = document.createElement("option");
+  cosmetics.textContent = "Cosmetics";
+  categoryList.add(cosmetics);
 
-let cosmetics = document.createElement("option");
-cosmetics.textContent = "Cosmetics";
-categoryList.add(cosmetics);
+  let clothes = document.createElement("option");
+  clothes.textContent = "Clothing";
+  categoryList.add(clothes);
 
-let clothes = document.createElement("option");
-clothes.textContent = "Clothing";
-categoryList.add(clothes);
-
-let other = document.createElement("option");
-other.textContent = "Other";
-categoryList.add(other);
-
+  let other = document.createElement("option");
+  other.textContent = "Other";
+  categoryList.add(other);
+}
+createCategoriesF();
 
 const itemButton = document.querySelector(".item-name-check");
 
-  // adding items to the list
-  itemButton.addEventListener("click", function () {
-    let selectedCategory = categoryList.value;
-    const itemInput = document.querySelector(".item-name").value;
-  
-    if (itemInput.value !== "") {
-      const listItem = document.createElement("dt");
-      listItem.textContent = itemInput;
-  
-      const list = document.querySelector(`.${selectedCategory}`);
-      list.appendChild(listItem);
+function updateTotalPrice() {
+  let totalPrice = 0;
+  for (let itemName in itemPrices) {
+    totalPrice += itemPrices[itemName];
+  }
+
+  if (totalPrice > budgetAmount) {
+    alert("Total price exceeds budget!");
+  }
+}
+
+// adding items to the list
+itemButton.addEventListener("click", function () {
+  let selectedCategory = categoryList.value;
+  const itemInput = document.querySelector(".item-name").value;
+
+  if (itemInput.value !== "") {
+    const listItem = document.createElement("dt");
+    listItem.textContent = itemInput;
+    listItem.classList.add("list-of-items");
+
+    const listBtn = document.createElement("button");
+    const infoBtn = document.createElement("button");
+    const infoList = document.createElement("dd");
+    const priceBtn = document.createElement("button");
+    const amountBtn = document.createElement("button");
+    const priceInput = document.createElement("input");
+    const amountInput = document.createElement("input");
+
+    listBtn.classList.add("delete-btn");
+    infoBtn.classList.add("info-btn");
+    priceBtn.classList.add("price-btn");
+    amountBtn.classList.add("amount-btn");
+
+    priceInput.type = "number";
+    priceInput.classList.add("price-input");
+    amountInput.type = "number";
+    amountInput.classList.add("amount-input");
+    infoBtn.textContent = "additional information";
+    listBtn.textContent = "Delete";
+
+    const list = document.querySelector(`.${selectedCategory}`);
+    removeHidden([`.${selectedCategory}`]);
+    list.appendChild(listItem);
+
+    listItem.appendChild(infoList);
+    infoList.appendChild(infoBtn);
+    infoList.appendChild(listBtn);
+
+    // adding more info about the items
+    const endAddingInfo = document.createElement("button");
+
+    infoBtn.addEventListener("click", function () {
+      infoList.appendChild(priceBtn);
+      priceBtn.textContent = "unit price (₺)";
+      priceBtn.appendChild(priceInput);
+      infoList.appendChild(amountBtn);
+      amountBtn.textContent = "amount";
+      amountBtn.appendChild(amountInput);
+      infoList.appendChild(endAddingInfo);
+      endAddingInfo.textContent = "Done";
+      endAddingInfo.classList.add("done-btn");
+
+      infoList.removeChild(infoBtn);
+    });
+
+    const displayInfo = document.createElement("span");
+    displayInfo.classList.add("displayed-info");
+    const changeInfoBtn = document.createElement("button");
+    changeInfoBtn.classList.add("change-info-btn");
+
+    endAddingInfo.addEventListener("click", () => {
+      infoList.removeChild(priceBtn);
+      priceBtn.removeChild(priceInput);
+      infoList.removeChild(amountBtn);
+      amountBtn.removeChild(amountInput);
+      infoList.removeChild(endAddingInfo);
+
+      // remove inputs and replace them with the values
+      let unitPrice = parseFloat(priceInput.value);
+      let amount = parseFloat(amountInput.value);
+      let itemTotalPrice = unitPrice * amount;
       
-      listItem.classList.add("list-of-items");
-      const listBtn = document.createElement("button");
-      listBtn.classList.add("delete-btn");
-      const infoBtn = document.createElement("button");
-      infoBtn.classList.add("info-btn");
-      
-      const infoList = document.createElement("dd");
-      const priceBtn = document.createElement("button");
-      priceBtn.classList.add("price-btn");
-      const amountBtn = document.createElement("button");
-      amountBtn.classList.add("amount-btn");
-      
-      const priceInput = document.createElement("input");
-      priceInput.type = "number";
-      priceInput.classList.add("price-input");
-      const amountInput = document.createElement("input");
-      amountInput.type = "number";
-      amountInput.classList.add("amount-input");
-      
-      listItem.appendChild(infoList)
-      infoList.appendChild(listBtn);
-      listBtn.textContent = "Delete";
-      infoList.appendChild(infoBtn);
-      infoBtn.textContent = "additional information";
-      
-      
-      // TO-DO adding more info about the items
-      const endAddingInfo = document.createElement("button");
-      
-      infoBtn.addEventListener("click", function () {
+      let itemName = itemInput;
+      itemPrices[itemName] = itemTotalPrice;
+      console.log(itemPrices)
+      updateTotalPrice();
+
+      infoList.appendChild(displayInfo);
+
+      if (amountInput.value !== "" && priceInput.value !== "") {
+        displayInfo.textContent = `total price: ${itemTotalPrice}(₺), amount: ${amount}`;
+        displayInfo.appendChild(changeInfoBtn);
+      } else if (amountInput.value !== "") {
+        displayInfo.textContent = `amount: ${amountInput.value}`;
+        displayInfo.appendChild(changeInfoBtn);
+      } else if (priceInput.value !== "") {
+        alert("Please enter your amount.");
+        infoList.appendChild(infoBtn);
+      } else {
+        listItem.appendChild(infoBtn);
+      }
+
+      changeInfoBtn.textContent = "change information";
+      changeInfoBtn.addEventListener("click", () => {
         infoList.appendChild(priceBtn);
-        priceBtn.textContent = "unit price (₺)";
+        priceBtn.textContent = "price (₺)";
         priceBtn.appendChild(priceInput);
         infoList.appendChild(amountBtn);
         amountBtn.textContent = "amount";
         amountBtn.appendChild(amountInput);
         infoList.appendChild(endAddingInfo);
         endAddingInfo.textContent = "Done";
-        endAddingInfo.classList.add("done-btn");
-        
-        infoList.removeChild(infoBtn);
+
+        displayInfo.removeChild(changeInfoBtn);
+        console.log(typeof unitPrice);
+        console.log(typeof amountInput.value);
+        console.log(amount);
       });
-      
-      const displayInfo = document.createElement("span");
-      displayInfo.classList.add("displayed-info");
-      const changeInfoBtn = document.createElement("button");
-      changeInfoBtn.classList.add("change-info-btn");
-      
-      endAddingInfo.addEventListener("click", () => {
-        infoList.removeChild(priceBtn);
-        priceBtn.removeChild(priceInput);
-        infoList.removeChild(amountBtn);
-        amountBtn.removeChild(amountInput);
-        infoList.removeChild(endAddingInfo);
-        
-        // remove inputs and replace them with the values
-        let unitPrice = priceInput.value;
-        let amount = amountInput.value;
-        let totalPrice = unitPrice * amountInput.value;
-        
-        infoList.appendChild(displayInfo);
-        
-        if (amountInput.value !== "" && priceInput.value !== "") {
-          displayInfo.textContent = `total price: ${totalPrice}(₺), amount: ${amount}`;
-          displayInfo.appendChild(changeInfoBtn);
-        } else if (amountInput.value !== "") {
-          displayInfo.textContent = `amount: ${amountInput.value}`;
-          displayInfo.appendChild(changeInfoBtn);
-        } else if (priceInput.value !== "") {
-          alert("Please enter your amount.")
-          infoList.appendChild(infoBtn);
-        } else {
-          listItem.appendChild(infoBtn);
-        }
-        
-        changeInfoBtn.textContent = "change information";
-        changeInfoBtn.addEventListener("click", () => {
-          infoList.appendChild(priceBtn);
-          priceBtn.textContent = "price (₺)";
-          priceBtn.appendChild(priceInput);
-          infoList.appendChild(amountBtn);
-          amountBtn.textContent = "amount";
-          amountBtn.appendChild(amountInput);
-          infoList.appendChild(endAddingInfo);
-          endAddingInfo.textContent = "Done";
-          
-          displayInfo.removeChild(changeInfoBtn);
-          console.log(typeof unitPrice);
-          console.log(typeof amountInput.value);
-          console.log(amount);
-        });
-      });
-      
-      // deleting items from the list
-      listBtn.addEventListener("click", () => {
-        list.removeChild(listItem);
-        items.indexOf(myItem);
-        let dItems = items.splice(items.indexOf(myItem), 1);
-        
-        deletedItems.push(...dItems);
-        
-        infoList.removeChild(displayInfo);
-      });
-    }
-  });
+    });
+
+    // deleting items from the list
+    listBtn.addEventListener("click", () => {
+      list.removeChild(listItem);
+      infoList.removeChild(displayInfo);
+    });
+  }
+});
